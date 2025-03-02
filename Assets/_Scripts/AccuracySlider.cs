7e3c40baa1a1bf7cc60cd9296a3935fdb55c7727
NikolaosComandariu <s283743@uos.ac.uk>
2025-03-02 17:45:19 +0000

    Refactored Accuracy Slider

--- Assets/_Scripts/AccuracySlider.cs
+++ Assets/_Scripts/AccuracySlider.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting.Dependencies.NCalc;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -13,43 +14,40 @@ public class AccuracySlider : MonoBehaviour
     private float _valueChange; 
     private float _sliderSpeed;
     private float _difficultyMultiplier = 1f;
+    private bool _isSliderPaused;
 
     private void Start()
     {
         _valueChange = BaseValueChange * _difficultyMultiplier;
+        _isSliderPaused = false;
     }
     
-    void Update()
+    private void Update()
     {
-        // PingPong moves between 0 and value length (here it is accuracySlider.maxValue) at a consistent rate. If you
-        // change the slider's minimum value in the editor to 0, and also change the animation curve to the default
-        // ease-in and ease-out curve, it removes the need to do negative value checks and use absolutes and signs. 
-        /* float pingPongValue = Mathf.PingPong(Time.time * 0.5f, accuracySlider.maxValue);
-        float newValue = lerpCurve.Evaluate(pingPongValue);
-        accuracySlider.value = newValue; */
-
-        // Animation curve uses 0 - 1 whereas the slider has a - 1 to 1 range, this deals with this issue by normalising the slider value.
-        float normalizedValue = (accuracySlider.value + 1) / 2f; 
-        float curveValue = lerpCurve.Evaluate(normalizedValue);
-
-        _sliderSpeed = curveValue * Mathf.Sign(_valueChange) * Mathf.Abs(_valueChange);
-
-        float nextValue = accuracySlider.value + _sliderSpeed;
-
-        // Changes speed from +ve to -ve by checking the next value of the slider. Setting the speed to 0 prevents it from going over the max and min.
-        if (nextValue > 1f || nextValue < -1f)
+        if (!_isSliderPaused)
         {
-            _valueChange = -_valueChange;
-            _sliderSpeed = 0;
+            CalculateValueChange();
         }
-
+        
         // For scoring, score ranges from 0 to 1, closer to the middle of the bar closer to a value of 1.
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            _valueChange = 0;
-            _accuracySliderValue = Mathf.Abs(accuracySlider.value); // Turn negatives into positives
-            _accuracySliderValue = 1 - _accuracySliderValue;
+            _isSliderPaused = true;
+            
+            // Calculate score when slider value is above or below the halfway mark.
+            if (accuracySlider.value > 0.5f)
+            {
+                _accuracySliderValue = accuracySlider.value;
+                _accuracySliderValue -= 0.5f;
+                _accuracySliderValue = 1 - (2 * _accuracySliderValue);
+            }
 
+            if (accuracySlider.value < 0.5f)
+            {
+                _accuracySliderValue = accuracySlider.value;
+                _accuracySliderValue = 2 * _accuracySliderValue;
+            }
+            
             _accuracyScore = _accuracySliderValue;
             Debug.Log(_accuracyScore);
         }
@@ -57,11 +55,11 @@ public class AccuracySlider : MonoBehaviour
         // For Testing
         if (Input.GetKeyDown(KeyCode.T))
         {
-            _valueChange = BaseValueChange * _difficultyMultiplier;
+            _isSliderPaused = false;
         }
     }
 
-    void FixedUpdate()
+    private void FixedUpdate()
     {
         accuracySlider.value = Mathf.Clamp(accuracySlider.value + _sliderSpeed, -1f, 1f);
     }
@@ -70,5 +68,12 @@ public class AccuracySlider : MonoBehaviour
     {
         _difficultyMultiplier = newMultiplier;
         _valueChange = BaseValueChange * _difficultyMultiplier; 
+    }
+
+    private void CalculateValueChange()
+    {
+        float pingPongValue = Mathf.PingPong(Time.time * 0.5f, accuracySlider.maxValue);
+        _valueChange = lerpCurve.Evaluate(pingPongValue);
+        accuracySlider.value = _valueChange;
     }
 }
