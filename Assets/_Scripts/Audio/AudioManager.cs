8c4217e832fd9a53020ae4f35be9782217c7dffe
NikolaosComandariu <s283743@uos.ac.uk>
2025-03-02 11:50:00 +0000

    Warning message sent if missing audio sources, audio sources added if they are missing

--- Assets/_Scripts/Audio/AudioManager.cs
+++ Assets/_Scripts/Audio/AudioManager.cs
@@ -1,21 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Audio;
 
 namespace TexasShootEm
 {
     public class AudioManager : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
-        {
+        [Header("Audio Sources")]
+        [SerializeField] private AudioSource sfxSource;
+        [SerializeField] private AudioSource musicSource;
         
-        }
+        [Header("Audio Mixer")]
+        [SerializeField] private AudioMixer audioMixer;
 
-        // Update is called once per frame
-        void Update()
+        private void Start()
         {
-        
+            if (!sfxSource)
+            {
+                Debug.LogWarning("No SFX Audio Source assigned!");
+                sfxSource = gameObject.AddComponent<AudioSource>();
+            }
+
+            if (!musicSource)
+            {
+                Debug.LogWarning("No Music Audio Source assigned!");
+                musicSource = gameObject.AddComponent<AudioSource>();
+            }
         }
     }
 }
