fd07f2f86e3cdef3cff11468981263532c258b56
Dan Hook <s188873@uos.ac.uk>
2025-03-02 20:30:23 +0000

    Pass vector2 value for OnDirectional input

--- Assets/_Scripts/InputReader.cs
+++ Assets/_Scripts/InputReader.cs
@@ -9,7 +9,7 @@ namespace TexasShootEm
     public class InputReader : ScriptableObject, GameInput.IGameplayActions
     {
         public event UnityAction OnAimEvent;
-        public event UnityAction OnDirectionalEvent;
+        public event UnityAction<Vector2> OnDirectionalEvent;
         
         private GameInput _gameInput;
         
@@ -39,7 +39,7 @@ namespace TexasShootEm
 
         public void OnDirectional(InputAction.CallbackContext context)
         {
-            OnDirectionalEvent?.Invoke();
+            OnDirectionalEvent?.Invoke(context.ReadValue<Vector2>());
         }
     }
 }
