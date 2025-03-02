17ab912ceffe45bfb4792e05086e11855737f690
Dan Hook <s188873@uos.ac.uk>
2025-03-02 20:25:54 +0000

    Remove Unity-centric code from random key generator

--- Assets/_Scripts/RandomKeyPressGenerator.cs
+++ Assets/_Scripts/RandomKeyPressGenerator.cs
@@ -1,17 +1,11 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using Random = UnityEngine.Random;
 
 namespace TexasShootEm
 {
-    public class RandomKeyPressGenerator : MonoBehaviour
+    public class RandomKeyPressGenerator
     {
-        [SerializeField] private int numberOfKeys = 2;
-
-        private enum Key { Up, Down, Left, Right }
         private static readonly Vector2[] DirectionAxes = new Vector2[]
         {
             new Vector2(0f, 1f),
@@ -21,40 +15,18 @@ namespace TexasShootEm
         };
         
         private Vector2 GetDirectionFromKey(Key key) => DirectionAxes[(int)key];
-        
-        private List<Key> _queuedKeys = new List<Key>();
 
-        private void Start()
+        public void GenerateKeys(ref List<Key> keys, int keyCount)
         {
-            GenerateKeys();
-        }
-
-        [SerializeField] private Text _text; 
-        
-        private void Update()
-        {
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                GenerateKeys();
-            }
-        }
-
-        private void GenerateKeys()
-        {
-            _queuedKeys.Clear();
-            _text.text = "";
+            keys.Clear();
             
-            for (int i = 0; i < numberOfKeys; i++)
+            for (int i = 0; i < keyCount; i++)
             {
                 Key randomKey = (Key) Random.Range(0, 4);
-                _queuedKeys.Add(randomKey);
-                _text.text += "\n" + randomKey.ToString();
-            }
-            
-            foreach (Key key in _queuedKeys)
-            {
-                Debug.Log(key);
+                keys.Add(randomKey);
             }
         }
     }
+    
+    public enum Key { Up, Down, Left, Right }
 }
